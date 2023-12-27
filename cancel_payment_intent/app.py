1f651b01cc2bbcc626fc9d1ebafcef268f4755ec
Spencer Romberg <33329110+S-Romberg@users.noreply.github.com>
2023-12-27 15:03:26 -0700

    added cancel payment intent function

---
+++ cancel_payment_intent/app.py
@@ -0,0 +1,53 @@
+import json
+import stripe
+import os
+
+stripe.api_key = os.environ.get('STRIPE_SECRET_KEY')
+
+def lambda_handler(event, context):
+    """Lambda function to cancel a payment intent"""
+
+    print("Received event: " + json.dumps(event, indent=2))
+    try:
+        body = json.loads(event['body'])
+        payment_intent_id = body['id']
+
+        # Retrieve the payment intent
+        payment_intent = stripe.PaymentIntent.retrieve(payment_intent_id)
+
+        # Cancel the payment intent
+        payment_intent.cancel()
+
+        return {
+            "statusCode": 200,
+            "body": json.dumps({
+                "paymentIntentId": payment_intent.id,
+                "status": payment_intent.status,
+            }),
+        }
+    except json.JSONDecodeError:
+        return {
+            "statusCode": 400,
+            "body": json.dumps({
+                "error": "Invalid JSON format in event body.",
+            }),
+        }
+    except stripe.error.StripeError as e:
+        # Handle Stripe errors
+        return {
+            "statusCode": 400,
+            "body": json.dumps({
+                "error": str(e),
+                "event": event,
+            }),
+        }
+
+    except Exception as e:
+        # Handle other unexpected errors
+        return {
+            "statusCode": 500,
+            "body": json.dumps({
+                "error": str(e),
+                "event": event,
+            }),
+        }
